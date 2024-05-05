33e2398dfe43c38a9c0c52efc9309ce8c20c92aa
Xqhare <xqhare@web.de>
2024-05-05 18:42:38 +0200

    added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,22 @@ mod tests;
 pub mod prelude {
     use std::{fs::File, io::Read};
     
+    /// Generates a cryptographically secure pseudorandom u8. Takes no arguments.
+    ///
+    /// ## Returns:
+    /// Returns `None` if the CSPRNG has no entropy available or no access to the CSPRNG was
+    /// possible.
+    /// Returns `Some(u8)` otherwise.
+    ///
+    /// ## Example:
+    /// ```
+    /// use tyche::prelude::random;
+    ///
+    /// fn main() {
+    ///   let random_number: u8 = random().unwrap();
+    ///   println!("Generated random u8: {}", random_number);
+    /// }
+    /// ```
     pub fn random() -> Option<u8> {
         let rng = File::open("/dev/urandom");
         if rng.is_ok() {
@@ -19,8 +35,21 @@ pub mod prelude {
     }
 
 
+    /// Call with the start and end of the range (both `usize`).
+    /// 
+    /// ## Returns
     /// Will return `None` if end is bigger than start, or if random() fails.
     /// Will return `Some` wrapping a number inside the given range.
+    ///
+    /// ## Example:
+    /// ```
+    /// use tyche::prelude::random_from_range;
+    ///
+    /// fn main() {
+    ///     let chosen_element = random_from_range(0, 100).unwrap();
+    ///     println!("Chosen element {chosen_element}, in range 0-100");
+    /// }
+    /// ```
     pub fn random_from_range(start: usize, end: usize) -> Option<usize> {
         let rng = File::open("/dev/urandom");
         if rng.is_ok() && start < end {
@@ -37,5 +66,4 @@ pub mod prelude {
         }
     }
 }
-
 
