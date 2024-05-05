aa99881454806cdbad01711302f7d9af9387fa22
Xqhare <xqhare@web.de>
2024-05-05 18:43:03 +0200

    tests for random_u8 and random_range

---
+++ src/tests.rs
@@ -0,0 +1,25 @@
+use crate::prelude::*;
+
+#[test]
+fn test_random_u8() {
+    for _ in 0..10000 {
+        let answ = random();
+        // println!("{:?}", answ);
+        assert!(answ.is_some());
+    }
+}
+
+#[test]
+fn test_random_range() {
+    for _ in 0..10000 {
+        let answ = random_from_range(0, 100);
+        // println!("{:?}", answ);
+        /* if answ.unwrap() == 100 {
+            println!("DID IT 100!!");
+        }
+        if answ.unwrap() == 0 {
+            println!("0! DID IT 0!");
+        } */
+        assert!(answ.is_some());
+    }
+}
