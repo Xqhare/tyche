ae59303a4947df13b7a0756619967c92d4d3c664
Xqhare <xqhare@web.de>
2024-05-05 16:22:15 +0200

    added first random function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+
+pub mod prelude {
+    use std::{fs::File, io::Read};
+    
+    pub fn random() -> Option<u8> {
+        let mut rng = File::open("/dev/urandom");
+        if rng.is_ok() {
+            let mut buffer = [0u8; 1];
+            if rng.unwrap().read_exact(&mut buffer).is_ok() {
+                return Some(buffer[0]);
+            } else {
+                return None;
+            }
+        } else {
+            return None;
+        }
+    }
 }
+
+
