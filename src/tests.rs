c35bd65ee8094912ce63d77feb87528c38ec53b3
Xqhare <xqhare@web.de>
2024-05-06 02:34:56 +0200

    added even more tests

--- src/tests.rs
+++ src/tests.rs
@@ -3,8 +3,17 @@ use crate::prelude::*;
 
 #[test]
 fn test_random_u8() {
-    for _ in 0..100000 {
-        let answ = random();
+    for _ in 0..1000000 {
+        let answ = random_u8();
+        // println!("{:?}", answ);
+        assert!(answ.is_some());
+    }
+}
+
+#[test]
+fn test_random_u16() {
+    for _ in 0..1000000 {
+        let answ = random_u16();
         // println!("{:?}", answ);
         assert!(answ.is_some());
     }
@@ -12,8 +21,17 @@ fn test_random_u8() {
 
 #[test]
 fn test_random_u64() {
-    for _ in 0..100000 {
+    for _ in 0..1000000 {
         let answ = random_u64();
+        // println!("{:?}", answ);
+        assert!(answ.is_some());
+    }
+}
+
+#[test]
+fn test_random_f32() {
+    for _ in 0..1000000 {
+        let answ = random_f32();
         // println!("{:?}", answ);
         assert!(answ.is_some());
     }
@@ -23,7 +41,7 @@ fn test_random_u64() {
 fn test_random_range() {
     let mut found0 = false;
     let mut found100 = false;
-    for _ in 0..100000 {
+    for _ in 0..1000000 {
         let answ = random_from_range(0, 100);
         // println!("{:?}", answ);
         if answ.unwrap() == 100 {
@@ -41,7 +59,7 @@ fn test_random_range() {
 
 #[test]
 fn test_random_floor() {
-    for n in 0..100000 {
+    for n in 0..1000000 {
         let answ = random_with_floor(n);
         // println!("{:?}", answ);
         assert!(answ.is_some());
@@ -50,7 +68,7 @@ fn test_random_floor() {
 
 #[test]
 fn test_random_ceiling() {
-    for n in 100000..200000  {
+    for n in 1000000..2000000  {
         let answ = random_with_ceiling(n);
         // println!("The random number between 0 and {} is: {}", n, answ.unwrap());
         assert!(answ.is_some());
@@ -59,21 +77,21 @@ fn test_random_ceiling() {
 
 #[test]
 fn test_random_index() {
-    let collection = (0..100).collect::<Vec<usize>>();
-    let mut found99 = false;
+    let collection = (0..1000).collect::<Vec<usize>>();
+    let mut found999 = false;
     let mut found0 = false;
-    for _ in 0..100000 {
+    for _ in 0..1000000 {
         let random_index = random_index(collection.len());
         assert!(random_index.is_some());
         // println!("{:?}", random_index);
         // These two tests test the same thing, I am more comfortable this way though.
-        assert!(random_index.unwrap() < 100);
+        assert!(random_index.unwrap() < 1000);
         let _index_test = collection[random_index.unwrap()];
         if random_index.unwrap() == 0 {
             found0 = true;
-        } else if random_index.unwrap() == 99 {
-            found99 = true;
+        } else if random_index.unwrap() == 999 {
+            found999 = true;
         }
     }
-    assert!(found0 && found99);
+    assert!(found0 && found999);
 }
